da5eb81927c4fde7e8eec71d79a1c653beef0b9d
Irina Lagutina <ivl77@yandex.ru>
2024-12-03 00:01:23 +0300

    Добавила интерфейс StorableEntity

--- project/libs/shared-types/src/index.ts
+++ project/libs/shared-types/src/index.ts
@@ -1,3 +1,5 @@
 export { Entity } from './lib/entity';
 export { AuthUser } from './lib/auth-user.interface';
 export { User } from './lib/user.interface';
+export { StorableEntity } from './lib/storable-entity.interface';
+export { EntityFactory } from './lib/entity-factory.interface';
