605cef41e9143e0b915a5110ff71f04b6694623f
Irina Lagutina <ivl77@yandex.ru>
2024-12-03 19:28:46 +0300

    Внедрили UserRepository в качестве зависимости в Authentification.module

--- project/libs/account/authentication/src/authentication-module/authentication.module.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '@project/user';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
+  imports: [UserModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
--- project/libs/account/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from '@project/user';
 
 @Injectable()
 export class AuthenticationService {
-
+  constructor(
+    private readonly userRepository: UserRepository
+  ) {}
 }
