7e0546723867b071594578b7454670ea45bb8f59
Irina Lagutina <ivl77@yandex.ru>
2024-12-08 16:55:13 +0300

    Научили UserRepository работать с mongo

--- project/libs/account/user/src/user-module/user.module.ts
+++ project/libs/account/user/src/user-module/user.module.ts
@@ -3,8 +3,13 @@ import { UserRepository } from './user.repository';
 import { UserFactory } from './user.factory';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModel, UserSchema } from './user.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: UserModel.name, schema: UserSchema }
+  ])],
   controllers: [UserController],
   providers: [UserRepository, UserFactory, UserService],
   exports: [UserRepository],
