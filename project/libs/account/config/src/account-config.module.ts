5d85c4f9d926702474ed1146ac5e12866cc4f04d
Irina Lagutina <ivl77@yandex.ru>
2024-12-11 12:20:34 +0300

    Вынесла путь в константу

--- project/libs/account/config/src/account-config.module.ts
+++ project/libs/account/config/src/account-config.module.ts
@@ -3,13 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import defaultApplicationConfig from './lib/app.config';
 import defaultMongodbConfig from './lib/mongo.config';
 
+const FILE_PATH = 'apps/account/account.env';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
       load: [defaultApplicationConfig, defaultMongodbConfig],
-      envFilePath: 'apps/account/account.env',
+      envFilePath: FILE_PATH,
     }),
   ]
 })
