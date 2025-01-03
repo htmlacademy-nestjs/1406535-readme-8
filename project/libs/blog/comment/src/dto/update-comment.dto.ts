134dd0b5e18e619576f31b991b0c4249ac1b435c
Irina Lagutina <ivl77@yandex.ru>
2025-01-03 13:46:45 +0500

    Добавила примеры запросов для модуля post

--- project/libs/blog/comment/src/dto/update-comment.dto.ts
+++ project/libs/blog/comment/src/dto/update-comment.dto.ts
@@ -1,12 +1,4 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateCommentDto } from './create-comment.dto';
-import { IsUUID } from 'class-validator';
 
-export class UpdateCommentDto extends PartialType(CreateCommentDto) {
-  @ApiProperty({
-    description: 'Comment ID',
-    example: '4e8a6ef4-2e28-46f6-ae5b-1d69b5dc1752'
-  })
-  @IsUUID()
-  public id: string;
-}
+export class UpdateCommentDto extends PartialType(CreateCommentDto) {}
